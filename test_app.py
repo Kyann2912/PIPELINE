73941e26cf5424f4d48c50dd1f9ad25ad9872cf4
Kyann2912 <kyann372@gmail.com>
2025-05-31 14:57:16 +0000

    Auto-commit du : 31-05-2025 à 14:57:16

--- test_app.py
+++ test_app.py
@@ -3,3 +3,6 @@ from app import addition
 def test_addition():
     assert addition(2, 3) == 9
     assert addition(-1, 1) == 0
+
+
+#.github/workflows/python-ci.yml – Pipeline CI avec GitHub Actions
