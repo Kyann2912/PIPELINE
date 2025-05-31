8efa1bfa7d248af91a2abca97c3f07e01998f53a
Kyann2912 <kyann372@gmail.com>
2025-05-31 14:58:28 +0000

    Auto-commit du : 31-05-2025 à 14:58:28

--- test_app.py
+++ test_app.py
@@ -6,3 +6,8 @@ def test_addition():
 
 
 #.github/workflows/python-ci.yml – Pipeline CI avec GitHub Actions
+
+
+#Voici un exemple concret de pipeline CI/CD simple, en Python,
+#  avec GitHub Actions, qui exécute automatiquement des 
+# tests unitaires chaque fois que tu pousses du code.
